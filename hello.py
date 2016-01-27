a77b9bed5b323e462c89d104c04596ba8840036a
cmis <cmis@cmis-hs16.(none)>
2016-01-27 11:12:56 +0700

    Python Power

---
+++ hello.py
@@ -0,0 +1,8 @@
+print "Hello, world!"
+print "HELP I'M STUCK IN A COMPUTER"
+print "SET ME FREEE"
+print "HELP ME"
+print "eh baows"
+print "Frogs in a Pickle"
+print 154146 7290
+print "Isaiah Jupo"
