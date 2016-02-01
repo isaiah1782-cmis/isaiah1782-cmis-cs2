675b9b1ed45f488fb786d536bad6d1cdf6d1b7ce
cmis <cmis@cmis-hs16.(none)>
2016-02-01 09:55:54 +0700

    Python

---
+++ assignment1.py
@@ -0,0 +1,17 @@
+Greet = "Hello!"
+myName = "Isaiah"
+myAgeInYears = 14.6587
+myHeightInMeters = 1.640899549188
+theLengthOf1SideOfASqareInMeters = 2
+theLengthOfARandomRectangleInMeters = 1.34850455476097207658
+theHeightOfARandomRectangleInMeters = 2.19087592496270760914
+
+
+print Greet
+print myName
+print myAgeInYears
+print myHeightInMeters
+print theLengthOf1SideOfASqareInMeters
+print theLengthOfARandomRectangleInMeters
+print theHeightOfARandomRectangleInMeters
+print myAgeInYears * 12
