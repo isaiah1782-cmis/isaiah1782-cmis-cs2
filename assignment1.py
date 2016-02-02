866a33f6d916a72beee1aeae723f2b76d13b8a04
cmis <cmis@cmis-hs16.(none)>
2016-02-02 11:52:41 +0700

    Paithong

--- assignment1.py
+++ assignment1.py
@@ -1,17 +1,32 @@
-Greet = "Hello!"
-myName = "Isaiah"
+myName = "Isaiah Thomas Jupo"
 myAgeInYears = 14.6587
 myHeightInMeters = 1.640899549188
-theLengthOf1SideOfASqareInMeters = 2
+theLengthOf1SideOfARandomSqareInMeters = 2
 theLengthOfARandomRectangleInMeters = 1.34850455476097207658
 theHeightOfARandomRectangleInMeters = 2.19087592496270760914
+myAgeInMonths = myAgeInYears * 12
+yearsIHaveLeftToLive = 100 - myAgeInYears
+myHeightInFeet = myHeightInMeters * 3.2808399
+theDifferenceOfMyHieghtInMetersForSomeoneOfMyAgeAndSexInMyHomeCountry = myHeightInMeters - 1.6
+theAreaOfARandomSquareInMeters = theLengthOf1SideOfARandomSqareInMeters * theLengthOf1SideOfARandomSqareInMeters
+theVolumeOfARandomCubeInMeters = theLengthOf1SideOfARandomSqareInMeters * theLengthOf1SideOfARandomSqareInMeters * theLengthOf1SideOfARandomSqareInMeters
+halfOfTheVolumeOfTheRandomCubeInMeters = theVolumeOfARandomCubeInMeters / 2
+oneNinthOfARectangleWithTheDimensionsIRandomlyMadeAbove = theLengthOfARandomRectangleInMeters * theHeightOfARandomRectangleInMeters / 9
 
 
-print Greet
 print myName
 print myAgeInYears
 print myHeightInMeters
-print theLengthOf1SideOfASqareInMeters
+print theLengthOf1SideOfARandomSqareInMeters
 print theLengthOfARandomRectangleInMeters
 print theHeightOfARandomRectangleInMeters
-print myAgeInYears * 12
+print myAgeInMonths
+print yearsIHaveLeftToLive
+print myHeightInFeet
+print theDifferenceOfMyHieghtInMetersForSomeoneOfMyAgeAndSexInMyHomeCountry
+print theAreaOfARandomSquareInMeters
+print theVolumeOfARandomCubeInMeters
+print halfOfTheVolumeOfTheRandomCubeInMeters
+print oneNinthOfARectangleWithTheDimensionsIRandomlyMadeAbove
+
+print "Hello." + " My name is " + myName + ". " + "I am about " + str(myAgeInYears) + " years old." + " My hieght (In Meters) is around " + str(myHeightInMeters) + ". "
