e2637d4fe58385652e2bfb576b6bc4119aed7051
cmis <cmis@cmis-hs16.(none)>
2016-02-05 09:55:54 +0700

    The human

---
+++ functions.py
@@ -0,0 +1,33 @@
+def Hello():
+    print "HELLO"
+    print "ITS ME"
+
+def Bye():
+    print "BYE"
+    print "NICE TO SEE YA"
+    print "GOD BLESS YOU"
+    print "WHY DID YOU LEAVE ME?"
+    print "~(.-.)~"
+
+def lol():
+    print
+    print "Traceback (most recent call last):"
+    print "  File '<sdin>', line 1"
+    print "    lol"
+    print "               ^"
+    print "SyntaxError: invalid syntax"
+
+lol()
+
+def The_human(action):
+    print "The_human" + action + "ed."
+
+Hello()
+x = 3
+y = 4
+print x * y * x * y * x
+The_human(" Walk")
+The_human(" Talk")
+The_human(" Explod")
+The_human(" Di")
+
