9974278135ce99b9a78aa3b43bffed1b378d832f
cmis <cmis@cmis-hs16.(none)>
2016-02-09 14:03:51 +0700

    Assignment 1

--- assignment1.py
+++ assignment1.py
@@ -1,36 +1,68 @@
 myName = "Isaiah Thomas Jupo"
+#I printed my name.
 myAgeInYears = 14.6587
+#I put my age in years as a decimal.
 myHeightInMeters = 1.640899549188
+#I put my height in meters.
 theLengthOf1SideOfARandomSqareInMeters = 2
+#I wrote down the length of 1 side of a random square in meters.
 theLengthOfARandomRectangleInMeters = 1.34850455476097207658
+#I wrote the length of a random rectangle in meters.
 theHeightOfARandomRectangleInMeters = 2.19087592496270760914
+#I wrote the hieght of a random rectangle in meters.
 myAgeInMonths = myAgeInYears * 12
+#I wrote my age in years times 12 so that I would get my age in months.
 yearsIHaveLeftToLive = 100 - myAgeInYears
+#I wrote 100 years minus my age in years so that I could calculate how many years I have left to live.
 myHeightInFeet = myHeightInMeters * 3.2808399
+#I found my height in feet by multiplying my height in meters by 3.2808399.
 theDifferenceOfMyHieghtInMetersForSomeoneOfMyAgeAndSexInMyHomeCountry = myHeightInMeters - 1.6
+#I found the difference of my height in meters for someone of the same age and sex in my home country.
 theAreaOfARandomSquareInMeters = theLengthOf1SideOfARandomSqareInMeters * theLengthOf1SideOfARandomSqareInMeters
+#I found the area of a random square in meters by multiplying the length of 1 side of the random square by the length of 1 side of the random square.
 theVolumeOfARandomCubeInMeters = theLengthOf1SideOfARandomSqareInMeters * theLengthOf1SideOfARandomSqareInMeters * theLengthOf1SideOfARandomSqareInMeters
+#I found the volume of a random cube in meters by muliplying the length of 1 side of a random square in meters by the length of 1 side of a random square in meters by the length of 1 side of a random square in meters.
 halfOfTheVolumeOfTheRandomCubeInMeters = theVolumeOfARandomCubeInMeters / 2
+#I found half of the volume of the random cube in meters by dividing the volume of a random cube in meters by 2.
 oneNinthOfARectangleWithTheDimensionsIRandomlyMadeAbove = theLengthOfARandomRectangleInMeters * theHeightOfARandomRectangleInMeters / 9
-
+#I found one ninth of a rectangle with the dimensions that I randomly made previously by multiplying the length of a random rectangle in meters by the height of a random rectangle in meters then divided it by 9.
 
 print myName
+#I instructed Python to print my name.
 print myAgeInYears
+#I instructed Python to print my age in years.
 print myHeightInMeters
+#I instructed Python to print my height in meters
 print theLengthOf1SideOfARandomSqareInMeters
+#I instructed Python to print the length of 1 side of a random square in meters.
 print theLengthOfARandomRectangleInMeters
+#I instructed Python to print the length of a random rectangle in meters.
 print theHeightOfARandomRectangleInMeters
+#I instructed Python to print the hieght of a random rectangle in meters.
 print myAgeInMonths
+#I instructed Python to print my age in months by multiplying my age in years by 12.
 print yearsIHaveLeftToLive
+#I instructed Python to print how many years I have left to live my life by doing 100 years minus my age in years so that I could calculate how many years I have left to live.
 print myHeightInFeet
+#I instructed Python to print my height in feet.
 print theDifferenceOfMyHieghtInMetersForSomeoneOfMyAgeAndSexInMyHomeCountry
+#I instructed Python to print the difference of my height in meters for someone of the same age and sex in my home country.
 print theAreaOfARandomSquareInMeters
+#I instructed Python to print the area of a random square in meters by multiplying the length of 1 side of the random square by the length of 1 side of the random square.
 print theVolumeOfARandomCubeInMeters
+#I instructed Python to print the volume of a random cube in meters by muliplying the length of 1 side of a random square in meters by the length of 1 side of a random square in meters by the length of 1 side of a random square in meters.
 print halfOfTheVolumeOfTheRandomCubeInMeters
+#I instructed Python to print half of the volume of the random cube in meters by dividing the volume of a random cube in meters by 2.
 print oneNinthOfARectangleWithTheDimensionsIRandomlyMadeAbove
+#I instructed Python to print one ninth of a rectangle with the dimensions that I randomly made previously by multiplying the length of a random rectangle in meters by the height of a random rectangle in meters then divided it by 9.
 
-print "Hello." + " My name is " + myName + ". " + "I am about " + str(myAgeInYears) + " years old." + " My hieght (In Meters) is around " + str(myHeightInMeters) + ". " + "My age in Months is " + str(myAgeInMonths) + ". "
+print "Hello." + " My name is " + myName + ". " + "I am about " + str(myAgeInYears) + " years old." + " My hieght (In Meters) is around " + str(myHeightInMeters) + ". " + "My age in Months is " + str(myAgeInMonths) + ". " + "If the average human dies at 100 years old, I have " + str(yearsIHaveLeftToLive) + " years left to live."
+#I instructed Python to print a message using 5 of the variables I created previously.
 
-print
+print "Hello." + " The length of a random square in meters is " + str(theLengthOf1SideOfARandomSqareInMeters) + ". " + "The height of a random rectangle in meters is " + str(theHeightOfARandomRectangleInMeters) + " and the the length of a random rectangle in meters is " + str(theLengthOfARandomRectangleInMeters) + ". " + "The area of a random square in meters is " + str(theAreaOfARandomSquareInMeters) + " and the volume if a random cube in meters is " + str(theVolumeOfARandomCubeInMeters) + "."
+#I instructed Python to print out a second message using 5 more of the variables that I created previously.
 
-print "HA" * 10000000
+print ";) " * 10000
+#I instructed Python to print
+print "Ha! Gadem"
+#I instructed Python to print a finishing line to make myslelf feel better.
