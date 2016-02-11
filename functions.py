fc42a0a58c503211956b9fcb68d49cf4de6841fb
cmis <cmis@cmis-hs16.(none)>
2016-02-11 09:53:24 +0700

    Functions

--- functions.py
+++ functions.py
@@ -1,33 +1,40 @@
-def Hello():
-    print "HELLO"
-    print "ITS ME"
+def add(x, y):
+	return x + y
+#I defined a function for each of the four basic math operations (+, -, *, /). I made each function took 2 arguments and returned the result.
 
-def Bye():
-    print "BYE"
-    print "NICE TO SEE YA"
-    print "GOD BLESS YOU"
-    print "WHY DID YOU LEAVE ME?"
-    print "~(.-.)~"
+def sub(x, y):
+    return x - y
+#I defined a function for each of the four basic math operations (+, -, *, /). I made each function took 2 arguments and returned the result.
 
-def lol():
-    print
-    print "Traceback (most recent call last):"
-    print "  File '<sdin>', line 1"
-    print "    lol"
-    print "               ^"
-    print "SyntaxError: invalid syntax"
+def mul(x, y):
+    return x * y
+#I defined a function for each of the four basic math operations (+, -, *, /). I made each function took 2 arguments and returned the result.
 
-lol()
+def div(x, y):
+    return x / y
+#I defined a function for each of the four basic math operations (+, -, *, /). I made each function took 2 arguments and returned the result.
 
-def The_human(action):
-    print "The_human" + action + "ed."
+def hours_from_seconds(seconds):
+    return seconds/3600
+#I defined a function that took a number of seconds and returned the number of hours.
 
-Hello()
-x = 3
-y = 4
-print x * y * x * y * x
-The_human(" Walk")
-The_human(" Talk")
-The_human(" Explod")
-The_human(" Di")
+def circle_area(radius):
+    return math.pi(radius)**2
+#I defined a function that took 1 argument that represented the radius of a circle. I made the function return the area of the circle.
 
+def sphere_volume(radius):
+    return 4/3 * math.pi(radius)**3
+#I defined a function that took 1 argument that represented the radius of a sphere and returned the volume.
+
+def avg_volume(x, y):
+    return ((4/3 * math.pi(x)**3)+(4/3 * math.pi(y)**3))/2
+#I defined a function that took 2 integers that represented the diameters of a sphere. I made the function return the average of the volumes.
+
+def area(x, y, z):
+    p =(x+y+z)/1
+    return math.sqrt(p*(p-x)(p-y)(p-z))
+#I defined a function that took the 3 side lengths of a triangle as arguments and returned the area.
+
+def right_align(abc):
+    len(abc) 
+#I defined a function that took a string as an argument and returned that word with additional spaces so that it was perfectly right aligned on the screen.
