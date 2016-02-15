9f4291c6d73ff8b225a04a81c9a7cec2ef4d8d9e
cmis <cmis@cmis-hs16.(none)>
2016-02-15 14:40:25 +0700

    Scratch work

---
+++ simple.py
@@ -0,0 +1,19 @@
+x = raw_input("Type a number: ")
+y = raw_input("Type another number: ")
+z = int(x) + int(y)
+print str(x) + "+" + str(y) + "=" + str(z)
+print " "
+print " "
+print " "
+a = raw_input("What is 19 - 10: ")
+b = raw_input("What is 19 - 9: ")
+c = int(a) + int(b)
+print str(a) + " + " + str(b) + " = 21"
+print "No, you stupid"
+print " "
+print " "
+print " "
+d = raw_input("What is the first letter of your name: ")
+e = raw_input("What is the first letter of your middle name: ")
+f = raw_input("What is the first letter of your last name: ")
+print "Your initials are: " + str(d) + "." + str(e) + "." + str(f) + "."
