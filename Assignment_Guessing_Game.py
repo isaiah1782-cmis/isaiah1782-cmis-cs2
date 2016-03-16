7cd383082495a09b7bf1844993a51ad62f572c18
cmis <cmis@cmis-hs16.(none)>
2016-03-16 11:55:32 +0700

    Updated

--- Assignment_Guessing_Game.py
+++ Assignment_Guessing_Game.py
@@ -1,13 +1,42 @@
 import math
 import random
 
-def Guessing(
+Minimum_Number = int(raw_input("What is the minimum number? "))
+Maximum_Number = int(raw_input("What is the maximum number? "))
 
-Minimum_Number = raw_input("What is the minimum number? ")
-Maximum_Number = raw_input("What is the maximum number? ")
+def Finding_a_Number():
+    number = random.randint(Minimum_Number, Maximum_Number)
+    return number
 
 print "I'm thinking of a number from " + str(Minimum_Number) + " to " + str(Maximum_Number) + "."
 
-Guess = raw_input("What do you think it is?: ")
+Guess = str(raw_input("What do you think it is?: "))
 
+print ""
+print "The target was " + str(Finding_a_Number()) + "."
+print "Your guess was " + str(Guess) + "."
 
+def Did_You_Guess_it_Correctly():
+    if Guess == str(Finding_a_Number()):
+        y = True
+    return y
+
+def You_Guessed_it():
+    if Did_You_Guess_it_Correctly() == True:
+        a = "Congrats!"
+    return a
+
+def Under_or_Over():
+    if str(Finding_a_Number()) > Guess:
+        return True
+    elif str(Finding_a_Number()) < Guess:
+        return False
+
+def Is_Under_or_Over_True_or_False():
+    if Under_or_Over() == True:
+        x = "That's over by "
+    if Under_or_Over() == False:
+        x = "That's under by "
+    return x
+
+print Is_Under_or_Over_True_or_False()
--- Magic_Trick.py
+++ Magic_Trick.py
@@ -1,13 +1,16 @@
 import math
+import random
 
 x = raw_input("What's your number guess?: ")
 
 def guess(x):
 
 	if x == 7:
-		print "You got that right!"
+		y = "You got that right!"
 	elif x == 6:
-		print "You got that wrong!"
-
+		y = "You got that wrong!"
+    else:
+        y = random.randit(0,10)
+    return y * random.random()
 
 
