878ec1f6e1ddb719f4673427825e99d6db9f1fb0
cmis <cmis@cmis-hs10.(none)>
2016-03-22 09:58:25 +0700

    Work in progress

---
+++ Text_Game.py
@@ -0,0 +1,23 @@
+import math
+import random
+
+print "Hello!"
+print ""
+
+def making_a_Random_Four_Digit_NumberOne():
+    Random_4_Digit_Number1 = int(raw_input("""Type a random four digit number (No decimals/fractions/negatives): """))
+
+x = making_a_Random_Four_Digit_NumbeOne()
+
+print x
+
+
+
+
+
+
+
+
+
+print "Ok so now I'm going to add my random number to your random number number."
+print "I'll say " + str(x) + "."
--- simple.py
+++ simple.py
@@ -7,14 +7,15 @@ def area(x, y, z):
     p = (x+y+z)/2
     return (p*(p-x)*(p-y)*(p-z))**0.5
 
-l = raw_input("What's the length?: ")
-w = raw_input("What's the width?: ")
-h = raw_input("What's the hieght?: ")
-units = raw_input("What's the measurement?(If you don't know then just say 'Units'): ")
+def main():
+    l = raw_input("What's the length?: ")
+    w = raw_input("What's the width?: ")
+    h = raw_input("What's the hieght?: ")
+    units = raw_input("What's the measurement?(If you don't know then just say 'Units'): ")
+    a = area(int(l), int(w), int(h))
+    print "The area of your triangle is " + str(a) + str(units) + ". "
 
-a = area(int(l), int(w), int(h))
-
-print "The area of your triangle is " + str(a) + str(units) + ". "
+main()
 
 print ""
 
@@ -24,9 +25,10 @@ print ""
 def sphere_volume(radius):
     return 4/3 * math.pi * (radius)**3
 
-r = raw_input("What's the radius?: ")
-units1 = raw_input("What's the measurement?(If you don't know then just say 'Units'): ")
+def main2():
+    r = raw_input("What's the radius?: ")
+    units1 = raw_input("What's the measurement?(If you don't know then just say 'Units'): ")
+    b = sphere_volume(int(r))
+    print "The volume of your sphere is " + str(b) + str(units1) + ". "
 
-b = sphere_volume(int(r))
-
-print "The volume of your sphere is " + str(b) + str(units1) + ". "
+main2()
