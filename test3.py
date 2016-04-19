241fc4600978e8db57585c723fb89877405fa6e1
Isaiah Thomas Jupo <isaiah1782@cmis.ac.th>
2016-04-19 18:48:28 +0700

    Update test3.py

--- test3.py
+++ test3.py
@@ -2,31 +2,31 @@ import string
 import random
 import time
 
-print "Hi! This program will ask you to type some text and then It will try to guess it by random characters!"
+print "Hi! This program will ask you to type some text and then it will try to guess your text by random characters!"
 print ""
 
-possibleCharacters = string.ascii_lowercase + string.digits + string.ascii_uppercase + ' .,!?/ ;:() *&^% $#@{} []`~ _=^-+"'
+a = string.ascii_lowercase + string.digits + string.ascii_uppercase + ' .,!?/ ;:() *&^% $#@{} []`~ _=^-+"'
 
 target = raw_input("Enter your text: ")
-attemptThis = ''.join(random.choice(possibleCharacters) for i in range(len(target)))
-attemptNext = ''
+b = ''.join(random.choice(a) for i in range(len(target)))
+c = ''
 
 completed = False
 
 generation = 0
 
 while completed == False:
-    print(attemptThis)
-    attemptNext = ''
+    print(b)
+    c = ''
     completed = True
     for i in range(len(target)):
-        if attemptThis[i] != target[i]:
+        if b[i] != target[i]:
             completed = False
-            attemptNext += random.choice(possibleCharacters)
+            c += random.choice(a)
         else:
-            attemptNext += target[i]
+            c += target[i]
     generation += 1
-    attemptThis = attemptNext
+    b = c
     time.sleep(0.1)
 
 print("Target matched! The computer took " + str(generation) + " try(ies)!")
