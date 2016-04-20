95f7b98d6ef6d67889f1fe87aa0334b8ab179262
cmis <cmis@cmis-hs10.(none)>
2016-04-20 14:24:43 +0700

    Done

--- test2.py
+++ test2.py
@@ -1,8 +1,10 @@
+n = int(raw_input("Count: "))
+
 def countdown(n):
-    if n <= 0:
+    if n == 10 or n >= 10:
         print 'Blastoff!'
     else:
         print n
-        countdown(n-1)
+        (countdown(n+1))
 
-countdown(10)
+countdown(n)
