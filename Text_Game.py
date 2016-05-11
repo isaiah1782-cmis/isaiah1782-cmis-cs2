f0abf725e0075e9cafd69b05403d6cddf7f652fc
cmis <cmis@cmis-hs10.(none)>
2016-05-11 09:55:35 +0700

    Updated

--- Text_Game.py
+++ Text_Game.py
@@ -2,34 +2,34 @@ import math
 import random
 import sys
 
-def main():
-    #Description:
-    #First: The friendly program asks you what your name is and what you want it to be called. Then it welcomes the user to it's program.
-    #Second: The Friendly program says a random strange number.
-    #Third: It puts down a random number and then asks the user to put in a random number and then it puts down another random number and then it asks the user to put down another random number and then it puts down a random number. If the user puts down something that isn't what the program asks for (including numbers that aren't creative), it will tell the user what they did wrong and how they should do it.
-    #Forth: It adds all the random numbers that were put down and then it gets a magic number.
-    #Fifth: It asks the user if the user was amused. If the user said that the user was confused, then it will explain.
-    Users_name = raw_input("Hello! What is your name?: ")
-    What_the_User_Wants_My_Name_To_Be = raw_input("What would you like to call me?(The friendly program): ")
-    print ""
-    print "Hello " + str(Users_name) + "! Welcome to " + str(What_the_User_Wants_My_Name_To_Be) + "'s Text Game! Enjoy! :D"
-    print ""
-    print ""
-    print "{0}".format(1782 + 31089)
-    print ""
-    print ""
+#Description:
+#First: The friendly program asks you what your name is and what you want it to be called. Then it welcomes the user to it's program.
+#Second: The Friendly program says a random strange number.
+#Third: It puts down a random number and then asks the user to put in a random number and then it puts down another random number and then it asks the user to put down another random number and then it puts down a random number. If the user puts down something that isn't what the program asks for (including numbers that aren't creative), it will tell the user what they did wrong and how they should do it.
+#Forth: It adds all the random numbers that were put down and then it gets a magic number.
+#Fifth: It asks the user if the user was amused. If the user said that the user was confused, then it will explain.
+Users_name = raw_input("Hello! What is your name?: ")
+What_the_User_Wants_My_Name_To_Be = raw_input("What would you like to call me?(The friendly program): ")
+print ""
+print "Hello " + str(Users_name) + "! Welcome to " + str(What_the_User_Wants_My_Name_To_Be) + "'s Text Game! Enjoy! :D"
+print ""
+print ""
+print "{0}".format(1782 + 31089)
+print ""
+print ""
 
-    First_Random_Number = random.randint(1000, 9999)
-    Last_Random_Number = ((1782 + 31089) - (1782 + 18216)) - int(First_Random_Number)
-    Most_Important_Number = random.random()
+First_Random_Number = random.randint(1000, 9999)
+Last_Random_Number = ((1782 + 31089) - (1782 + 18216)) - int(First_Random_Number)
+Most_Important_Number = random.random()
 
-    print "So to start us off, I'm going to generate a random number."
-    print "Hmm.... I'll put down " + str(First_Random_Number) + "."
+print "So to start us off, I'm going to generate a random number."
+print "Hmm.... I'll put down " + str(First_Random_Number) + "."
 
-    Users_Random_Four_Digit_Number = int(raw_input("""Now """ + str(Users_name) + """, please type a random four digit number (NO decimals/fractions/negatives and by saying "random" I want you to get creative): """))
+Users_Random_Four_Digit_Number = int(raw_input("""Now """ + str(Users_name) + """, please type a random four digit number (NO decimals/fractions/negatives and by saying "random" I want you to get creative): """))
 
-    print ""
+print ""
 
+def making_a_Random_Four_Digit_Number():
     if Users_Random_Four_Digit_Number < 1000 or Users_Random_Four_Digit_Number > 10000:
         answer = 0
         print """Um """ + str(Users_name) + """, """ +  """ " """ + str(Users_Random_Four_Digit_Number) + """ " """ + """ is not a four digit number. An example of a four digit number is 1782 NOT 123 or 12345. Nice try though."""
@@ -43,16 +43,20 @@ def main():
         print "Ok good! Now I'm going to generate another random number."
         print "The randomly generated number is " + str(answer) + "."
         Answer = random.randint(1000, 9999)
+    return answer
 
-    x = Answer #Programmer's Checkpoint
+x = making_a_Random_Four_Digit_Number() #Programmer's Checkpoint
 
-    Users_Random_Four_Digit_Number2 = int(raw_input("""So now """ + str(Users_name) + """, please type another random four digit number (NO decimals/fractions/negatives and by saying "random" I want you to get creative): """))
+Users_Random_Four_Digit_Number2 = int(raw_input("""So now """ + str(Users_name) + """, please type another random four digit number (NO decimals/fractions/negatives and by saying "random" I want you to get creative): """))
 
+def if_the_user_said_the_same_thing_twice():
     if int(Users_Random_Four_Digit_Number) == int(Users_Random_Four_Digit_Number2):
         print ""
         leave = sys.exit("You have said that before, " + str(Users_name) + ". Please redo the game again, " + str(Users_name) +  ".")
 
+if_the_user_said_the_same_thing_twice()
 
+def making_a_Random_Four_Digit_Number2():
     if Users_Random_Four_Digit_Number2 < 1000 or Users_Random_Four_Digit_Number2 > 10000:
         answer = 0
         print """Um """ + str(Users_name) + """, """ +  """ " """ + str(Users_Random_Four_Digit_Number2) + """ " """ + """ is not a four digit number. An example of a four digit number is 1782 NOT 123 or 12345. Nice try though."""
@@ -67,27 +71,28 @@ def main():
         answer = 9999 - Users_Random_Four_Digit_Number2
         print "Ok good! Again, I'm going to generate another random number."
         print "The randomly generated number is " + str(answer) + "."
-        Answer1 = random.randint(1000, 9999)
+        Answer = random.randint(1000, 9999)
+    return answer
 
-    y = Answer1 #Programmer's Checkpoint
+y = making_a_Random_Four_Digit_Number2() #Programmer's Checkpoint
 
-    print ""
-    print "Ok! Now we're almost done. I am now going to generate the final random number."
-    print "I will say " + str(Last_Random_Number) + "!"
-    print ""
+print ""
+print "Ok! Now we're almost done. I am now going to generate the final random number."
+print "I will say " + str(Last_Random_Number) + "!"
+print ""
 
-    print """Yay! We are now done!
-Now... Time for the magic!
-You see, if we add all those numbers that we put down (You can use a calculator)... what do we get?
+print "Yay! We are now done!"
+print "Now... Time for the magic!"
+print "You see, if we add all those numbers that we put down (You can use a calculator)... what do we get?"
+print ""
+print ""
+print "We get " + str(int(First_Random_Number) + int(Users_Random_Four_Digit_Number) + int(x) + int(Users_Random_Four_Digit_Number2) + int(y) + int(Last_Random_Number)) + "!"
+print ""
+print ""
 
+If_User_Thought_That_The_Text_Game_Was_Cool = raw_input("Did you like the game?(You can type 'Yes', 'No', or 'I didn't understand'): ")
 
-"""
-    print "We get " + str(int(First_Random_Number) + int(Users_Random_Four_Digit_Number) + int(x) + int(Users_Random_Four_Digit_Number2) + int(y) + int(Last_Random_Number)) + "!"
-    print ""
-    print ""
-
-    If_User_Thought_That_The_Text_Game_Was_Cool = raw_input("Did you like the game?(You can type 'Yes', 'No', or 'I didn't understand'): ")
-
+def If_User_Thought_That_The_Text_Game_Was_Cool_Response():
     if str(If_User_Thought_That_The_Text_Game_Was_Cool) == "Yes" or str(If_User_Thought_That_The_Text_Game_Was_Cool) == "yes" or str(If_User_Thought_That_The_Text_Game_Was_Cool) == "YES":
         print ""
         print "Great! My Job here is now done. Thanks for participating, " + str(Users_name) + ", and have a good day! :)"
@@ -104,5 +109,6 @@ You see, if we add all those numbers that we put down (You can use a calculator)
         print "What was that number that I said from the beginning?"
         No = True
         x = bool(No)
+    return No
 
-main()
+If_User_Thought_That_The_Text_Game_Was_Cool_Response()
