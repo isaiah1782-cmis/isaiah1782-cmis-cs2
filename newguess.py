0242a2c45c45f5de619e64cfbedb2103cc125e99
cmis <cmis@cmis-hs10.(none)>
2016-05-12 11:55:35 +0700

    OOh... Saucy

--- newguess.py
+++ newguess.py
@@ -2,8 +2,7 @@ import random
 
 print "You have 3 rounds to guess 3 numbers between 0 and 100. The program will tell you if you're you guess is higher than the number and if you're guess is lower than the number. You have 10 tries for each round"
 
-def main(Number_of_Tries):
-    The_Number = random.randint(0, 100)
+def main(Number_of_Tries, The_Number):
     Users_Guess = int(raw_input("What's your guess?: "))
     if Number_of_Tries == 0:
         print "You have run out of tries! You are still awesome though."
@@ -11,22 +10,29 @@ def main(Number_of_Tries):
         print "Congratulations! You have guessed the number!"
     elif Users_Guess > The_Number:
         print "Thats too high"
-        main(Number_of_Tries - 1)
+        main(Number_of_Tries - 1, The_Number)
     elif Users_Guess < The_Number:
         print "Thats too low"
-        main(Number_of_Tries - 1)
+        main(Number_of_Tries - 1, The_Number)
+
+import time
+start_time = time.time()
 
 print ""
 print "Round 1"
 print ""
-main(10)
+main(10, random.randint(0, 100))
 
 print ""
 print "Round 2"
 print ""
-main(10)
+main(10, random.randint(0, 100))
 
 print ""
 print "Round 3"
 print ""
-main(10)
+main(10, random.randint(0, 100))
+
+
+
+print("You took %s seconds to finish!" % (time.time() - start_time))
