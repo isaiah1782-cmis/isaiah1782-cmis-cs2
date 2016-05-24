004efe93ff7184bd2988f7cc806f51248463f33d
cmis <cmis@cmis-hs10.(none)>
2016-05-24 09:55:57 +0700

    Watermelon

---
+++ whileloops.py
@@ -0,0 +1,19 @@
+def countdown(x):
+    while x >= 0:
+        print x
+        x -= 1
+
+#countdown(10)
+#coutndown(23)
+
+def counter(x):
+    while x <= 0:
+        print x
+        x += 1
+    while x >= 0:
+        print x
+        x -= 1
+
+
+counter(10)
+counter(-10)
