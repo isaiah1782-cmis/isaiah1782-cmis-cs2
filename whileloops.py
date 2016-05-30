2fe361eb83b196c65d5ce03a0a01706e3253de85
cmis <cmis@cmis-hs10.(none)>
2016-05-30 09:55:30 +0700

    Updated

--- whileloops.py
+++ whileloops.py
@@ -8,20 +8,62 @@ def countup_or_countdown(x, y):
             print x
             x += 1
 
-countup_or_countdown(int(raw_input("Start: ")), int(raw_input("End: ")))
+#countup_or_countdown(int(raw_input("Start: ")), int(raw_input("End: ")))
 
 print ""
 
 def sum0f0dds(n):
-    while n % 2 == 0:
-        continue #WRONG
-    if n > 0:
+    odds = 0
+    count = 0
+    if n % 2 == 0:
+        odds = 0
+        n += 0
+        count += 0
+    elif n > 0:
         while n < 1000:
             print n
             n += 1
-    if n < 0:
+            odds += n
+            count += 1
+    elif n < 0:
         while n > -1000:
             print n
-            n +- 1
+            n -= 1
+            odds += n
+            count += 1
+    print "The total of all the odds is " + str(odds) + "."
+    print "The number of odds is " + str(count) + "."
+    print "The average of the odds is " + str(int(odds/count)) + "."
 
-sum0f0dds(int(raw_input("Number: ")))
+#sum0f0dds(int(raw_input("Number (From -1000 to 1000): ")))
+
+print ""
+
+def grid(width, height, character):
+    out = ""
+    while width > 0:
+        out += character
+        width -= 1
+    return out
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print grid(int(raw_input("Width: ")), int(raw_input("Height: ")), raw_input("Character: "))
